95fedbcec5fcf9f262ba100345c8977ca64e530b
crvernon <chrisrvernon@gmail.com>
2022-04-22 22:30:21 -0400

    add tests for set_alignment

---
+++ statemodify/tests/test_utils.py
@@ -0,0 +1,56 @@
+import unittest
+
+import statemodify.utils as utils
+
+
+class TestUtils(unittest.TestCase):
+    """Tests to ensure functionality and stability of utils.py."""
+
+    COMP_RIGHT = "    test"
+    COMP_LEFT = "test    "
+    COMP_NONE = "test"
+
+    def test_set_alignment(self):
+        """Tests for the set_alignment function."""
+
+        # ensure error is raised when value other than right or left is passed
+        with self.assertRaises(AssertionError):
+            s = utils.set_alignment(value="test",
+                                    n_spaces=0,
+                                    align="center")
+
+        # check for left padding (right alignment) of four on value; total length of eight
+        s = utils.set_alignment(value="test",
+                                n_spaces=4,
+                                align="right")
+
+        self.assertEqual(self.COMP_RIGHT, s)
+        self.assertEqual(len(self.COMP_RIGHT), len(s))
+
+        # check for right padding (left alignment) of four on value; total length of eight
+        s = utils.set_alignment(value="test",
+                                n_spaces=4,
+                                align="left")
+
+        self.assertEqual(self.COMP_LEFT, s)
+        self.assertEqual(len(self.COMP_LEFT), len(s))
+
+        # check for no space of four on value; total length of eight
+        s = utils.set_alignment(value="test",
+                                n_spaces=0,
+                                align="left")
+
+        self.assertEqual(self.COMP_NONE, s)
+        self.assertEqual(len(self.COMP_NONE), len(s))
+
+        # check for n_spaces smaller than length of value of four; total length of eight
+        s = utils.set_alignment(value="test",
+                                n_spaces=-2,
+                                align="left")
+
+        self.assertEqual(self.COMP_NONE, s)
+        self.assertEqual(len(self.COMP_NONE), len(s))
+
+
+if __name__ == '__main__':
+    unittest.main()
