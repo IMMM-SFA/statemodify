fe18226dbc0a5385e1b596618a7720e7a5cb388c
Chris Vernon <chrisrvernon@gmail.com>
2022-05-04 12:30:52 -0400

    add fn to pad value with spaces

--- statemodify/utils.py
+++ statemodify/utils.py
@@ -30,3 +30,40 @@ def set_alignment(value: str,
 
     else:
         raise AssertionError(f"Choice for alignment '{align}' not supported.  Must be 'left' or 'right'.")
+
+
+def pad_with_spaces(value: str,
+                    expected_width: int,
+                    align="left") -> str:
+    """Pad a string with the number of spaces specified by the user.
+
+    :param value:                   Value to evaluate.
+    :type value:                    str
+
+    :param expected_width:          Expected width of the field.
+    :type expected_width:           int
+
+    :param align:                   Either 'left' or 'right' alignment for the value.
+    :type align:                    str
+
+    :return:                        Value with string padding.
+
+    """
+
+    # strip all whitespace padding from value
+    value_stripped = value.strip()
+
+    # get length of data in field
+    field_length = len(value_stripped)
+
+    if field_length <= expected_width:
+
+        # get the number of missing spaces
+        missing_spaces = expected_width - field_length
+
+        return set_alignment(value=value_stripped,
+                             n_spaces=missing_spaces,
+                             align=align)
+
+    else:
+        raise AssertionError(f"Column width '{field_length}' exceeds the expected width '{expected_width}'")
