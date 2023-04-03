c5846bdaa977b8f0efa5dcf62ea0cbcecde9e628
crvernon <chrisrvernon@gmail.com>
2023-04-03 18:34:42 -0400

    add sampling fn for parallel

--- statemodify/res.py
+++ statemodify/res.py
@@ -1,8 +1,11 @@
 import os
+import pkg_resources
 from typing import Union, List
 
 import numpy as np
+from joblib import Parallel, delayed
 
+import statemodify.sampler as sampler
 import statemodify.utils as utx
 
 
@@ -200,3 +203,77 @@ def modify_single_res(output_dir: str,
             out.write(item)
 
 
+def modify_res(output_dir: str,
+               scenario: str,
+               basin_name: str = "Gunnison",
+               template_file: Union[None, str] = None,
+               data_specification_file: Union[None, str] = None,
+               target_structure_id_list: Union[None, List[str]] = None,
+               skip_rows: int = 0,
+               seed_value: Union[None, int] = None,
+               n_jobs: int = -1,
+               n_samples: int = 1):
+    """Modify a single reservoir (.res) file based on a user provided sample.
+
+    :param output_dir:          Path to output directory.
+    :type output_dir:           str
+
+    :param scenario:            Scenario name.
+    :type scenario:             str
+
+    :param sample:              An array of samples for each parameter.
+    :type sample:               np.array
+
+    :param sample_id:           Numeric ID of sample that is being processed. Defaults to 0.
+    :type sample_id:            int
+
+    :param basin_name:                      Name of basin for either:
+                                                Upper_Colorado
+                                                Yampa
+                                                San_Juan
+                                                Gunnison
+                                                White
+    :type basin_name:                       str
+
+    :param template_file:       If a full path to a template file is provided it will be used.  Otherwise the
+                                default template in this package will be used.
+    :type template_file:        Union[None, str]
+
+    :param data_specification_file:     If a full path to a data specification template is provided it will be used.
+                                        Otherwise, the default file in the package is used.
+    :type data_specification_file:      Union[None, str]
+
+    :param target_structure_id_list:    Structure id list to process.  If None, all structure ids will be processed.
+    :type target_structure_id_list:     Union[None, List[str]]
+
+    :param skip_rows:           Number of rows to skip after the commented fields end; default 1
+    :type skip_rows:            int, optional
+
+    :param seed_value:                      Integer to use for random seed or None if not desired
+    :type seed_value:                       Union[None, int] = None)
+
+    :param n_jobs:                          Number of jobs to process in parallel.  Defaults to -1 meaning
+                                            all but 1 processor.
+    :type n_jobs:                           int
+
+    :param n_samples:                       Used if generate_samples is True.  Number of samples to generate.
+    :type n_samples:                        int
+
+    """
+
+    yaml_file = pkg_resources.resource_filename("statemodify", "data/parameter_definitions.yml")
+    param_dict = utx.yaml_to_dict(yaml_file)
+
+    # generate an array of samples to process
+    sample_array = sampler.generate_sample_all_params(n_samples=n_samples,
+                                                      seed_value=seed_value)
+
+    # generate all files in parallel
+    results = Parallel(n_jobs=n_jobs, backend="loky")(delayed(modify_single_res)(output_dir=output_dir,
+                                                                                 scenario=scenario,
+                                                                                 sample=sample[param_dict["rstorage"]["index"]],
+                                                                                 sample_id=sample_id,
+                                                                                 template_file=template_file,
+                                                                                 data_specification_file=data_specification_file,
+                                                                                 target_structure_id_list=target_structure_id_list,
+                                                                                 skip_rows=skip_rows) for sample_id, sample in enumerate(sample_array))
