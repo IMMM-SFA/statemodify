b414cf0c7c82ceddba33fa0d0e01a6b9af755684
crvernon <chrisrvernon@gmail.com>
2023-04-06 10:04:37 -0400

    fix template test

--- statemodify/tests/test_utils.py
+++ statemodify/tests/test_utils.py
@@ -1,3 +1,4 @@
+import os
 import pkg_resources
 import unittest
 
@@ -23,9 +24,10 @@ class TestUtils(unittest.TestCase):
     def test_select_template_file(self):
         """Test template file retrieval."""
 
-        template_file_custom = utx.select_template_file("/test/template_file.eva")
+        basin_name = "Upper_Colorado"
+        template_file_custom = os.path.basename(utx.select_template_file(basin_name, None, "eva"))
 
-        self.assertEqual("/test/template_file.eva", template_file_custom, msg="Failure for template file selection.")
+        self.assertEqual("cm2015.eva", template_file_custom, msg="Failure for template file selection.")
 
     def test_yaml_str(self):
         """Test YAML return string."""
