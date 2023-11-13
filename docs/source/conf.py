3782bf24ee65547091de74416980abdc538a59d2
crvernon <chrisrvernon@gmail.com>
2023-11-13 14:59:52 -0500

    remove unneeded refs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,9 +96,9 @@ html_logo = 'images/logo_statemodify.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs. This file should be a Windows icon file (.ico), 16x16 or 32x32 pixels.
-html_favicon = '_static/favicon.ico'
+# html_favicon = '_static/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named 'default.css' will overwrite the builtin 'default.css'.
-html_static_path = ['_static']
+# html_static_path = ['_static']
